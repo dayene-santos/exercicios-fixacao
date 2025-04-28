eeebbe4405f4565cfd01e9d427d4e68fbf850615
DayeneS <dayene-santos@hotmail.com>
2025-04-27 22:22:22 -0300

    Exercicio 04 Tipos Primitivos - Operadores Aritméticos

---
+++ src/TiposPrimitivosOperadores/dominio/exercicio05.java
@@ -0,0 +1,19 @@
+package TiposPrimitivosOperadores.dominio;
+
+public class exercicio05 {
+
+    /* Operadores Aritméticos
+
+    Imagine que você está desenvolvendo uma calculadora simples.
+    Implemente uma função que receba dois números e aplique as quatro operações básicas (+, -, *, /).
+
+     */
+    /**
+     * Operadores aritméticos:
+     * + soma
+     * - subtração
+     * * multiplicação
+     * / divisão
+     */
+
+}
---
+++ src/TiposPrimitivosOperadores/teste/exercicio05.java
@@ -0,0 +1,22 @@
+package TiposPrimitivosOperadores.teste;
+
+public class exercicio05 {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+
+        int soma, subtracao, multiplicacao, divisao;
+
+        soma = x + y;
+        subtracao = x - y;
+        multiplicacao = x * y;
+        divisao = x / y;
+
+        System.out.println("A soma dos valores é: " + soma);
+        System.out.println("A diferença dos valores é: " + subtracao);
+        System.out.println("O produto dos valores é: " + multiplicacao);
+        System.out.println("A divisão dos valores é: " + divisao);
+
+    }
+}
