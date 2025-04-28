6469517a7968a1d1b4cb67b961e10ecf7b7b3c70
DayeneS <dayene-santos@hotmail.com>
2025-04-27 22:12:58 -0300

    Exercicio 04 Tipos Primitivos - Strings

---
+++ src/TiposPrimitivosOperadores/dominio/exercicio04.java
@@ -0,0 +1,12 @@
+package TiposPrimitivosOperadores.dominio;
+
+public class exercicio04 {
+    /*
+     Strings
+    Ana está desenvolvendo um formulário e precisa manipular textos.
+    Explique o que é o tipo `String` em Java e mostre um exemplo de como concatenar duas Strings diferentes.
+
+    Em Java, o tipo String é uma classe que representa uma sequência de caracteres (texto).
+    Ela é usada para armazenar e manipular texto, como palavras, frases e qualquer outro tipo de conteúdo alfanumérico.
+     */
+}
---
+++ src/TiposPrimitivosOperadores/teste/exercicio04.java
@@ -0,0 +1,13 @@
+package TiposPrimitivosOperadores.teste;
+
+public class exercicio04 {
+    public static void main(String[] args) {
+
+        String nome = "Dayene";
+        String sobrenome = "Santos";
+        String endereco = "Palmares - BH/MG";
+
+        System.out.println("Meu nome completo é: " + nome + " " + sobrenome);
+        System.out.println("Moro em: " + endereco);
+    }
+}
